b464ca0bb37bb634ad0fa52a2241476f1e6ee483
bangquan.qian <chainz@foxmail.com>
2019-06-18 19:55:11 +0800

    commit

---
+++ src/main/java/cn/tenbit/hare/core/exechain/HareExecutable.java
@@ -0,0 +1,11 @@
+package cn.tenbit.hare.core.exechain;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2019-04-15 23:44
+ */
+
+public interface HareExecutable<T> {
+
+    void execute(T target) throws Exception;
+}
