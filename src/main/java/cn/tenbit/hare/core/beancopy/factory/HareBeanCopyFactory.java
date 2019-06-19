9c37c05691c530feaf8e589c6524ecaaf03a2ce5
bangquan.qian <chainz@foxmail.com>
2019-06-19 16:53:18 +0800

    Update HareBeanCopyFactory.java

--- src/main/java/cn/tenbit/hare/core/beancopy/factory/HareBeanCopyFactory.java
+++ src/main/java/cn/tenbit/hare/core/beancopy/factory/HareBeanCopyFactory.java
@@ -1,8 +1,15 @@
 package cn.tenbit.hare.core.beancopy.factory;
 
 import cn.tenbit.hare.core.beancopy.HareBeanCopy;
+import cn.tenbit.hare.core.beancopy.adaptor.HareBeanCopyAdaptor;
 import cn.tenbit.hare.core.beancopy.constant.HareBeanCopyConsts;
 import cn.tenbit.hare.core.beancopy.constant.HareBeanCopyTypeEnums;
+import cn.tenbit.hare.core.beancopy.copier.HareCustomBeanCopier;
+import cn.tenbit.hare.core.beancopy.support.HareCglibBeanCopy;
+import cn.tenbit.hare.core.beancopy.support.HareCommonsBeanCopy;
+import cn.tenbit.hare.core.beancopy.support.HareSpringBeanCopy;
+import cn.tenbit.hare.core.common.constant.HareConsts;
+import cn.tenbit.hare.core.common.unify.HareCommonHolder;
 import cn.tenbit.hare.core.util.HareAssertUtils;
 
 /**
@@ -27,6 +34,7 @@ public class HareBeanCopyFactory {
         return getBeanCopy(HareBeanCopyTypeEnums.COMMONS_BEAN_UTILS);
     }
 
+    @SuppressWarnings(HareConsts.SUPPRESS_WARNING_UNCHECKED)
     private static HareBeanCopy getBeanCopy(HareBeanCopyTypeEnums enu) {
         HareAssertUtils.notNull(enu);
 
@@ -35,14 +43,18 @@ public class HareBeanCopyFactory {
             default:
                 break;
             case CUSTOM:
+                hareBeanCopy = new HareCustomBeanCopier();
                 break;
             case CGLIB_BEAN_COPIER:
+                hareBeanCopy = new HareCglibBeanCopy();
                 break;
             case COMMONS_BEAN_UTILS:
+                hareBeanCopy = new HareCommonsBeanCopy();
                 break;
             case SPRING_BEAN_UTILS:
+                hareBeanCopy = new HareSpringBeanCopy();
                 break;
         }
-        return hareBeanCopy;
+        return new HareBeanCopyAdaptor(new HareCommonHolder<>(hareBeanCopy));
     }
 }
